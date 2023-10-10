0ab627e0568dc920463cb2ab33ac378b3df98e7f
Gabriel <gabrielfariaw@gmail.com>
2023-10-09 21:58:55 -0300

    Finish project generator QR Code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,42 @@
 const container = document.querySelector('.container');
-const qrCodeBtn = document.querySelector('#qr-code button');
+const qrCodeBtn = document.querySelector('#qr-form button');
+const qrCodeInput = document.querySelector('#qr-form input');
+const qrCodeImg = document.querySelector('#qr-code img')
 
-// Events
+// Events 
+
+// Generate QR Code
+function generateQrCode(){
+   const qrCodeInputValue = qrCodeInput.value;
+
+  if(!qrCodeInputValue) return;
+
+  qrCodeBtn.innerHTML = "Generating code..."
+
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+  qrCodeImg.addEventListener('load', () => {
+    container.classList.add("active");
+    qrCodeBtn.innerHTML = "Code create!"
+  });
+}
+
+
+qrCodeBtn.addEventListener('click', () => {
+    generateQrCode();
+});
+
+qrCodeInput.addEventListener('keydown', (e) => {
+    if(e.code === "Enter"){
+        generateQrCode();
+    }
+});
+
+// Clean QR Code
+
+qrCodeInput.addEventListener("keyup", () => {
+    if(!qrCodeInput.value) {
+        container.classList.remove("active");
+        qrCodeBtn.innerHTML = "Generate QR Code"
+    }
+});
---
+++ redme.md
@@ -0,0 +1,35 @@
+# QR Code Generator
+
+This is a project that allows users to create QR codes from URLs or text. With it, you can easily generate QR codes to conveniently share information.
+
+## Contents
+
+- [How It Works](#how-it-works)
+- [Technologies Used](#technologies-used)
+- [How to Use](#how-to-use)
+- [Contributing](#contributing)
+- [License](#license)
+
+## How It Works
+
+The project consists of a simple web page that provides the functionality to generate QR codes from URLs or text. Key features include:
+
+- A user-friendly interface with an input field to enter the URL or text you want to encode.
+- A button to generate the QR code.
+- Display of the generated QR code on the page for download or use.
+
+## Technologies Used
+
+This project is built using the following technologies:
+
+- HTML: for structuring the web page.
+- CSS: for styling the interface.
+- JavaScript: for handling user interaction and generating the QR code.
+- Google Fonts library: for styling the typography of the page.
+
+## How to Use
+
+1. Clone the repository to your computer:
+
+   ```bash
+   git clone https://github.com/your-username/your-project.git
